6ac161085290e92bc37e056edbbc6f4b621f83ed
walter <wutong@ihealthlabs.com.cn>
2016-05-02 23:53:42 +0800

    删除bootstrap card 完善item card的material版本

--- client/modules/items/components/Item.jsx
+++ client/modules/items/components/Item.jsx
@@ -8,8 +8,11 @@ import CardMedia from 'material-ui/lib/card/card-media';
 import CardTitle from 'material-ui/lib/card/card-title';
 import FlatButton from 'material-ui/lib/flat-button';
 import CardText from 'material-ui/lib/card/card-text';
+import Toggle from 'material-ui/lib/toggle';
 
 import moment from 'moment';
+
+
 
 class Item extends React.Component {
     render() {
@@ -19,53 +22,47 @@ class Item extends React.Component {
         const completedStyle = item.complete ? {'color': '#eee', 'text-decoration': 'line-through'}
             : {'color': '#333'};
         const compactstyle = Object.assign(style, completedStyle);
+        const toggleStyles = {
+            block: {
+                maxWidth: 100,
+            },
+            toggle: {
+                marginBottom: 16,
+            }
+        };
+        const categoryDisplayText = item.get_category_name() ? item.get_category_name() : "其他"
+        const dueDisplayText = item.due ? ' - ' + moment(Number(item.due)).format('MM/DD/YYYY') : ""
         return (
             <Col xs={12} sm={6} md={4}>
-                <Card>
-                    <CardTitle title={item.name} subtitle={item.get_category_name() ? item.get_category_name() : "其他"} />
+                <Card  style={compactstyle}>
+                    <CardTitle title={item.name} subtitle={categoryDisplayText+dueDisplayText} />
+                    <CardText>{item.description}</CardText>
                     <CardActions>
-                        <FlatButton href={`/edit/${item._id}`} label="编辑" />
-                        <FlatButton label="Action2" />
+                        <div style={toggleStyles.block}>
+                            <Toggle
+                                    label="已完成"
+                                    style={toggleStyles.toggle}
+                                    toggled={item.complete}
+                                    ref="complete"
+                                    defaultToggled={item.complete}
+                                    onToggle = {this.markComplete.bind(this)} />
+                        </div>
+                        <FlatButton linkButton={true} href={`/edit/${item._id}`} secondary={true} label="编辑" />
+                        <FlatButton onClick={this.removeItem.bind(this)} primary={true} label="删除" />
                     </CardActions>
                 </Card>
-
-
-                <Panel style={compactstyle}>
-                    <Row>
-                        <Col xs={10}>
-                            <h2 style={completedStyle}>{item.name}<small> - {item.get_category_name()}</small></h2>
-
-                        </Col>
-                        <Col xs={1}>
-                            <a href={`/edit/${item._id}`}><Glyphicon glyph="pencil"></Glyphicon></a>
-                        </Col>
-                        <Col xs={1}>
-                            <a href="#" onClick={this.removeItem.bind(this)}><Glyphicon glyph="remove"></Glyphicon></a>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={12}>
-                            <p>{item.description} {item.due ? '- ' + moment(Number(item.due)).format('MM/DD/YYYY') : ""}</p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={12}>
-                            <Input key={item._id} checked={item.complete} ref="complete" onChange={this.markComplete.bind(this)} type="checkbox" label="完成?"/>
-                        </Col>
-                    </Row>
-                </Panel>
             </Col>
         )
     }
 
     markComplete() {
-        const complete = this.refs.complete.getChecked();
+        const complete = !this.props.item.complete;
         const itemId = this.props.item._id;
         Meteor.call('items.markComplete', complete, itemId);
     }
 
     removeItem(e) {
-        e.preventDefault()
+        e.preventDefault();
         const itemId = this.props.item._id;
         Meteor.call('items.softRemove', itemId);
     }
